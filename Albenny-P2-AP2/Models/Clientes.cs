a00b540b998fab40aa0ea2527a56f24133099f2c
Albenny Hernandez Cruz <albenny_hernandez@ucne.edu.do>
2021-06-29 22:29:14 -0400

    Agregar archivos de proyecto.

---
+++ Albenny-P2-AP2/Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Albenny_P2_AP2.Models
+{
+    public class Clientes
+    {
+    
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+    }
+}
+
