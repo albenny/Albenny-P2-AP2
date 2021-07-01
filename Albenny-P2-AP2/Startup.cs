ed3c5e83d4f43a84ff4bfb68db6ed40897a274b6
Albenny Hernandez Cruz <albenny_hernandez@ucne.edu.do>
2021-06-30 20:57:58 -0400

    HACIENDO LA MIGRACION

--- Albenny-P2-AP2/Startup.cs
+++ Albenny-P2-AP2/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Albenny_P2_AP2.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.Toast;
 
 namespace Albenny_P2_AP2
 {
@@ -29,6 +31,7 @@ namespace Albenny_P2_AP2
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
